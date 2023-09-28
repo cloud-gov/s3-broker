a17b9a038fe0d4a32094b065f45bfab15b718465
Mark Boyd <mark.boyd@gsa.gov>
2023-09-28 11:52:57 -0400

    update gingko version

--- awsiam/iam_user_test.go
+++ awsiam/iam_user_test.go
@@ -3,7 +3,7 @@ package awsiam_test
 import (
 	"errors"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	. "github.com/cloudfoundry-community/s3-broker/awsiam"
