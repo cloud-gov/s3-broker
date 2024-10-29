5d1a888ceca8ceb91c3c284690c94dfb7bdb15ec
Stephen Greenberg <stephen.greenberg@gsa.gov>
2024-10-29 06:44:49 -0600

    us-east-1 option doesn't exist

--- provider/aws_provider.go
+++ provider/aws_provider.go
@@ -5,11 +5,5 @@ type AwsProvider struct {
 }
 
 func (a *AwsProvider) Endpoint() string {
-	var endpoint string
-	if a.region == "us-east-1" {
-		endpoint = "s3-fips.amazonaws.com"
-	} else {
-		endpoint = "s3-fips." + a.region + ".amazonaws.com"
-	}
-	return endpoint
+	return "s3-fips." + a.region + ".amazonaws.com"
 }
