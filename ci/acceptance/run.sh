7fe719d368444780365ed97bf41bacd6ca6414cf
Mark Boyd <mark.boyd@gsa.gov>
2026-02-18 10:16:00 -0500

    update acceptance test script to handle services with additional instances

--- ci/acceptance/run.sh
+++ ci/acceptance/run.sh
@@ -2,15 +2,15 @@
 
 set -eux
 
-cf api ${CF_API_URL}
-(set +x; cf auth ${CF_USERNAME} ${CF_PASSWORD})
+cf api "${CF_API_URL}"
+(set +x; cf auth "$CF_USERNAME" "$CF_PASSWORD")
 
-cf target -o "${CF_ORGANIZATION}"
+cf target -o "$CF_ORGANIZATION"
 
-cf target -s "${CF_SPACE}"
+cf target -s "$CF_SPACE"
 
 pushd broker-src/ci/acceptance
-  cf push ${APP_NAME} -m ${MEMORY_LIMIT:-"128M"} --no-start
+  cf push "$APP_NAME" -m "${MEMORY_LIMIT:-"128M"}" --no-start
 popd
 
 cf set-env "${APP_NAME}" SERVICE_NAME "${SERVICE_NAME}"
@@ -20,20 +20,26 @@ cf set-env "${APP_NAME}" ADDITIONAL_INSTANCE_NAME "${ADDITIONAL_INSTANCE_NAME:-}
 cf set-env "${APP_NAME}" ENCRYPTION "${ENCRYPTION:-""}"
 cf create-service "${SERVICE_NAME}" "${PLAN_NAME}" "${SERVICE_INSTANCE_NAME}"
 
-cf bind-service ${APP_NAME} ${SERVICE_INSTANCE_NAME}
-cf start ${APP_NAME}
+if [[ -n "$ADDITIONAL_INSTANCE_NAME" ]]; then
+  cf create-service "${SERVICE_NAME}" "${PLAN_NAME}" "${ADDITIONAL_INSTANCE_NAME}"
+  cf bind-service "${APP_NAME}" "${SERVICE_INSTANCE_NAME}" -c '{"additional_instances": ["'"$ADDITIONAL_INSTANCE_NAME"'"]}'
+else
+  cf bind-service "${APP_NAME}" "${SERVICE_INSTANCE_NAME}"
+fi
 
-url=$(cf app ${APP_NAME} | grep -e "urls: " -e "routes: " | awk '{print $2}')
+cf start "$APP_NAME"
+
+url=$(cf app "$APP_NAME" | grep -e "urls: " -e "routes: " | awk '{print $2}')
 status=$(curl -w "%{http_code}" "https://${url}")
 if [ "${status}" != "200" ]; then
   echo "Unexpected status code ${status}"
-  cf logs ${APP_NAME} --recent
+  cf logs "$APP_NAME" --recent
   exit 1
 fi
 
-cf create-service-key ${SERVICE_INSTANCE_NAME} ${SERVICE_INSTANCE_NAME}-service-key
-cf service-keys ${SERVICE_INSTANCE_NAME} 
-cf delete-service-key -f ${SERVICE_INSTANCE_NAME} ${SERVICE_INSTANCE_NAME}-service-key
+cf create-service-key "${SERVICE_INSTANCE_NAME}" "${SERVICE_INSTANCE_NAME}-service-key"
+cf service-keys "${SERVICE_INSTANCE_NAME}"
+cf delete-service-key -f "${SERVICE_INSTANCE_NAME}" "${SERVICE_INSTANCE_NAME}-service-key"
 
 cf delete -f "${APP_NAME}"
 cf delete-service -f "${SERVICE_INSTANCE_NAME}"
