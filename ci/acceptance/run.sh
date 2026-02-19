dadec97701ca1cd0d4fef380498e50f86d0d0eb1
Mark Boyd <mark.boyd@gsa.gov>
2026-02-19 12:09:46 -0500

    update logic for handling ADDITIONAL_INSTANCE_NAME variable in CI test script

--- ci/acceptance/run.sh
+++ ci/acceptance/run.sh
@@ -13,20 +13,19 @@ pushd broker-src/ci/acceptance
   cf push "$APP_NAME" -m "${MEMORY_LIMIT:-"128M"}" --no-start
 popd
 
-cf set-env "${APP_NAME}" SERVICE_NAME "${SERVICE_NAME}"
-cf set-env "${APP_NAME}" IS_PUBLIC "${IS_PUBLIC:-"false"}"
-cf set-env "${APP_NAME}" IS_DELETE "${IS_DELETE:-"false"}"
-cf set-env "${APP_NAME}" ADDITIONAL_INSTANCE_NAME "${ADDITIONAL_INSTANCE_NAME:-}"
-cf set-env "${APP_NAME}" ENCRYPTION "${ENCRYPTION:-""}"
-cf create-service "${SERVICE_NAME}" "${PLAN_NAME}" "${SERVICE_INSTANCE_NAME}"
+cf set-env "$APP_NAME" SERVICE_NAME "$SERVICE_NAME"
+cf set-env "$APP_NAME" IS_PUBLIC "${IS_PUBLIC:-"false"}"
+cf set-env "$APP_NAME" IS_DELETE "${IS_DELETE:-"false"}"
+cf set-env "$APP_NAME" ENCRYPTION "${ENCRYPTION:-""}"
+cf create-service "$SERVICE_NAME" "$PLAN_NAME" "$SERVICE_INSTANCE_NAME"
 
-if [ -z "${ADDITIONAL_INSTANCE_NAME+x}" ]; then
-  if [[ -n "$ADDITIONAL_INSTANCE_NAME" ]]; then
-    cf create-service "${SERVICE_NAME}" "${PLAN_NAME}" "${ADDITIONAL_INSTANCE_NAME}"
-    cf bind-service "${APP_NAME}" "${SERVICE_INSTANCE_NAME}" -c '{"additional_instances": ["'"$ADDITIONAL_INSTANCE_NAME"'"]}'
-  fi
+ADDITIONAL_INSTANCE_NAME="${ADDITIONAL_INSTANCE_NAME:-}"
+if [[ -n "$ADDITIONAL_INSTANCE_NAME" ]]; then
+  cf set-env "$APP_NAME" ADDITIONAL_INSTANCE_NAME "$ADDITIONAL_INSTANCE_NAME"
+  cf create-service "$SERVICE_NAME" "$PLAN_NAME" "$ADDITIONAL_INSTANCE_NAME"
+  cf bind-service "$APP_NAME" "$SERVICE_INSTANCE_NAME" -c '{"additional_instances": ["'"$ADDITIONAL_INSTANCE_NAME"'"]}'
 else
-  cf bind-service "${APP_NAME}" "${SERVICE_INSTANCE_NAME}"
+  cf bind-service "$APP_NAME" "$SERVICE_INSTANCE_NAME"
 fi
 
 cf start "$APP_NAME"
@@ -39,10 +38,10 @@ if [ "${status}" != "200" ]; then
   exit 1
 fi
 
-cf create-service-key "${SERVICE_INSTANCE_NAME}" "${SERVICE_INSTANCE_NAME}-service-key"
-cf service-keys "${SERVICE_INSTANCE_NAME}"
-cf delete-service-key -f "${SERVICE_INSTANCE_NAME}" "${SERVICE_INSTANCE_NAME}-service-key"
+cf create-service-key "$SERVICE_INSTANCE_NAME" "${SERVICE_INSTANCE_NAME}-service-key"
+cf service-keys "$SERVICE_INSTANCE_NAME"
+cf delete-service-key -f "$SERVICE_INSTANCE_NAME" "${SERVICE_INSTANCE_NAME}-service-key"
 
-cf delete -f "${APP_NAME}"
-cf delete-service -f "${SERVICE_INSTANCE_NAME}"
+cf delete -f "$APP_NAME"
+cf delete-service -f "$SERVICE_INSTANCE_NAME"
 
